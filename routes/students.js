a04b0a9af2ef3910093d4b7c03a9fa2f9451ed6f
Paxren666 <annikakoyle@u.boisestate.edu>
2025-10-24 15:54:47 -0600

    Implemented all Functions of students.js

--- routes/students.js
+++ routes/students.js
@@ -2,7 +2,6 @@ let express = require('express');
 let router = express.Router();
 const db = require("./../db");
 
-
 /**
  * GET /students
  *
@@ -10,9 +9,21 @@ const db = require("./../db");
  */
 router.get("/students", async function (req, res)
 {
-    // TODO: implement this route
-});
+    try
+    {
+        const listOfStudents = await db.getAllStudents();
+        console.log("listOfStudents:", listOfStudents);
 
+
+        // Automatically converts array of students to JSON
+        res.send(listOfStudents);
+    }
+    catch (err)
+    {
+        console.error("Error:", err.message);
+        res.status(500).json({ "error": "Internal Server Error" });
+    }
+});
 
 /**
  * GET /students/{id}
@@ -24,8 +35,38 @@ router.get("/students", async function (req, res)
 router.get("/students/:id", async function (req, res)
 {
     // TODO: implement this route
-});
+    try
+    {
+        const id = req.params.id;
+        console.log("id = " + id);
 
+
+        const studentWithID = await db.getStudentWithId(id);
+        console.log("studentWithID:", studentWithID);
+
+
+        if (studentWithID == null)
+        {
+            console.log("No student with id " + id + " exists.");
+
+
+            // return 404 status code (i.e., error that the student was not found)
+            res.status(404).json({ "error": "student with id " + id + " not found" });
+            return;
+        }
+
+
+        // this automatically converts the student to JSON and returns it to the client
+        res.send(studentWithID);
+    }
+    catch (err)
+    {
+        console.error("Error:", err.message);
+        res.status(500).json({ "error": "Internal Server Error" });
+    }
+
+
+});
 
 /**
  * POST /students
@@ -42,8 +83,67 @@ router.get("/students/:id", async function (req, res)
 router.post("/students", async function (req, res)
 {
     // TODO: implement this route
-});
+    try
+    {
+        const firstName = req.body.firstName;
+        const lastName = req.body.lastName;
+        const birthDate = req.body.birthDate;
 
+
+        console.log("firstName = " + firstName);
+        console.log("lastName  = " + lastName);
+        console.log("birthDate = " + birthDate);
+
+
+        if (firstName === undefined)
+        {
+            res.status(400).json({ "error": "bad request: expected parameter 'firstName' is not defined" });
+            return;
+        }
+
+
+        if (lastName === undefined)
+        {
+            res.status(400).json({ "error": "bad request: expected parameter 'lastName' is not defined" });
+            return;
+        }
+
+
+        if (birthDate === undefined)
+        {
+            res.status(400).json({ "error": "bad request: expected parameter 'birthDate' is not defined" });
+            return;
+        }
+
+
+        // additional validation: birthDate format (ISO)
+        if (!/^\d{4}-\d{2}-\d{2}$/.test(birthDate))
+        {
+            res.status(422).json({ "error": "birthDate must be in ISO format yyyy-mm-dd" });
+            return;
+        }
+
+
+        let createdStudent = {
+            id: null, // will be set after inserting into database
+            firstName: firstName,
+            lastName: lastName,
+            birthDate: birthDate
+        };
+
+
+        createdStudent = await db.addNewStudent(createdStudent);
+
+
+        // return 201 status code (created)
+        res.status(201).json(createdStudent);
+    }
+    catch (err)
+    {
+        console.error("Error:", err.message);
+        res.status(422).json({ "error": "failed to add new student to the database" });
+    }
+});
 
 /**
  * PUT /students/{id}
@@ -64,7 +164,6 @@ router.put("/students/:id", async function (req, res)
     // TODO: implement this route or the PATCH route below
 });
 
-
 /**
  * PATCH /students/{id}
  * with the following optional form parameters:
@@ -82,8 +181,63 @@ router.put("/students/:id", async function (req, res)
 router.patch("/students/:id", async function (req, res)
 {
     // TODO: implement this route or the PUT route above
-});
+    try
+    {
+        const id = req.params.id;
+        const firstName = req.body.firstName || null;
+        const lastName = req.body.lastName || null;
+        const birthDate = req.body.birthDate || null;
 
+
+        console.log("id        = " + id);
+        console.log("firstName = " + firstName);
+        console.log("lastName  = " + lastName);
+        console.log("birthDate = " + birthDate);
+
+
+        // retrieve existing student
+        let studentToUpdate = await db.getStudentWithId(id);
+        console.log({ studentToUpdate });
+
+
+        if (studentToUpdate == null)
+        {
+            console.log("No student with id " + id + " exists.");
+            res.status(404).json({
+                "error": "failed to update the student with id = " + id + " in the database because it does not exist"
+            });
+            return;
+        }
+
+
+        // override existing fields with the ones provided in the request
+        if (firstName != null)
+        {
+            studentToUpdate.firstName = firstName;
+        }
+        if (lastName != null)
+        {
+            studentToUpdate.lastName = lastName;
+        }
+        if (birthDate != null)
+        {
+            studentToUpdate.birthDate = birthDate;
+        }
+
+
+        await db.updateExistingStudentInformation(studentToUpdate);
+
+
+        res.json(studentToUpdate);
+    }
+    catch (err)
+    {
+        console.error("Error:", err.message);
+        res.status(422).json({
+            "error": "failed to update the student with id = " + req.params.id + " in the database"
+        });
+    }
+});
 
 /**
  * DELETE /students/{id}
@@ -95,7 +249,41 @@ router.patch("/students/:id", async function (req, res)
 router.delete("/students/:id", async function (req, res)
 {
     // TODO: implement this route
-});
+    try
+    {
+        const id = req.params.id;
+        console.log("id = " + id);
 
+
+        const studentToDelete = await db.getStudentWithId(id);
+        console.log({ studentToDelete });
+
+
+        if (studentToDelete == null)
+        {
+            console.log("No student with id " + id + " exists.");
+
+
+            // return 404 status code (i.e., error that the student was not found)
+            res.status(404).json({
+                "error": "failed to delete the student with id = " + id + " from the database because it does not exist"
+            });
+            return;
+        }
+
+
+        await db.deleteExistingStudent(id);
+
+
+        res.status(204).send(); // there is no return value
+    }
+    catch (err)
+    {
+        console.error("Error:", err.message);
+        res.status(422).json({
+            "error": "failed to delete the student with id = " + req.params.id + " from the database"
+        });
+    }
+});
 
 module.exports = router;
